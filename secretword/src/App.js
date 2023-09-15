50ce62b9524158a12ef729c03e2f2984c7d9f73b
igor poncell <igorponcell@MBP-de-igor.local>
2023-09-15 15:04:04 -0300

    initial

---
+++ secretword/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StartScreen from '../../../CursoReact/secretword/src/components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+        <StartScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ secretword/src/data/words.js
@@ -0,0 +1,5 @@
+export const wordsList = {
+    corpo: ['perna', 'orelha', 'mão', 'cabeça', 'joelho', 'tornozelo'],
+    computador: ['teclado', 'mouse', 'monitor', 'webcam'],
+    animais: ['gato', 'coelho', 'vaca', 'ovelha', 'camelo', 'macaco'],
+}
